5d18a84d04d1a7b9f34f293bcabb5caea8dd5676
nour.khaled <nour.khaled@arup.com>
2019-12-24 12:25:10 +0000

    - Split the data into 70% training, 15% validation and 15% test - Train, validate and test the best classification svm model - Generate classification report to show accuracy, recall, precision and F1 score measures

--- A1/gender_classifier.py
+++ A1/gender_classifier.py
@@ -1,10 +1,9 @@
 import argparse
 import pickle
 
-import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import accuracy_score
-from sklearn.metrics import confusion_matrix
+from sklearn.metrics import classification_report
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
 
@@ -18,21 +17,22 @@ def transform_images_to_features_data(csv_file_path, gender_column_index, image_
     return X, Y
 
 
-def split_training_test_data(X, Y):
-    return train_test_split(X, Y, train_size=0.8)
+def split_training_test_data(X, Y, testsize):
+    return train_test_split(X, Y, test_size=testsize)
 
 # sklearn functions implementation
 
 
 def img_SVM(training_images, training_labels, test_images, test_labels):
 
-    classifier = SVC(kernel='poly')
+    classifier = SVC(kernel='poly', gamma='scale')
     classifier.fit(training_images, training_labels)
     pred = classifier.predict(test_images)
     print("Accuracy:", accuracy_score(test_labels, pred))
-
+    print(classification_report(test_labels, pred))
 
     return pred
+
 
 if __name__ == '__main__': ## command J
 
@@ -73,16 +73,6 @@ if __name__ == '__main__': ## command J
     with open(filename, 'rb') as f:
         X, Y = pickle.load(f)
 
-    tr_X, te_X, tr_Y, te_Y = split_training_test_data(X, Y)
-    pred = img_SVM(tr_X.reshape((3840, 68*2)), list(zip(*tr_Y))[0], te_X.reshape((960, 68*2)), list(zip(*te_Y))[0])
-
-    cm = confusion_matrix(te_Y, pred)
-    plt.matshow(cm)
-    plt.title('Confusion Matrix ')
-    plt.colorbar()
-    plt.ylabel('True Label')
-    plt.xlabel('Predicted Label')
-    plt.show()
-    plt.figure(figsize=(5,5))
-    plt.savefig("ConfusionMatrixSVM")
-    
+    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.3)
+    X_test, X_val, Y_test, Y_val = train_test_split(X_test, Y_test, test_size=0.5)
+    pred = img_SVM(X_train.reshape((3360, 68*2)), list(zip(*Y_train))[0], X_test.reshape((720, 68*2)), list(zip(*Y_test))[0])
