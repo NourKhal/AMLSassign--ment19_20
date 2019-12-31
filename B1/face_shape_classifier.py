af4c12e195384c90c6a78fb917389c6c2d21c9bb
nour.khaled <nour.khaled@arup.com>
2019-12-31 15:05:40 +0000

    Extract image features from the cartoon set - Write the extracted features to pickle file - Load the features by unserializing the pickled file

--- B1/face_shape_classifier.py
+++ B1/face_shape_classifier.py
@@ -1,23 +1,22 @@
 import argparse
-import A1.lab2_landmarks as l2
-from sklearn.model_selection import train_test_split
-import pickle
 import os
+import pickle
 import sys
-import numpy as np
+
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-import codecs
-import pandas as pd
+import A1.lab2_landmarks as l2
+import numpy as np
+import tensorflow.compat.v1 as tf
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 
 
-# def transform_images_to_features_data(csv_file_path, smiling_column_index, image_dir, face_landmarks_path):
-#
-#     X, y = l2.extract_features_labels(csv_file_path, smiling_column_index, image_dir, face_landmarks_path)
-#     Y = np.array([y, -(y - 1)]).T
-#     return X, Y
+def transform_images_to_features_data(csv_file_path, smiling_column_index, image_dir, face_landmarks_path):
 
-
+    X, y = l2.extract_features_labels(csv_file_path, smiling_column_index, image_dir, face_landmarks_path)
+    Y = np.array([y, -(y - 1)]).T
+    return X, Y
 
 
 if __name__ == '__main__':
@@ -33,7 +32,7 @@ if __name__ == '__main__':
                             help='the path to the face landmarks file',
                             required=True)
     arg_parser.add_argument('-fsi', '--face-shape-index',
-                            help='the index of the smiling column in the labels.csv file',
+                            help='the index of the face shape column in the labels.csv file',
                             required=True,
                             type=int)
     arg_parser.add_argument('-pd', '--preprocessed-data-file',
@@ -52,18 +51,16 @@ if __name__ == '__main__':
                                                                            face_shape_index,
                                                                            preprocessed_data_file))
 
-    # X, Y = transform_images_to_features_data(labels_file, face_shape_index, image_directory, landmarks_file)
-    # x_y = list(zip(X, Y))
+    X, Y = transform_images_to_features_data(labels_file, face_shape_index, image_directory, landmarks_file)
+    x_y = list(zip(X, Y))
 
-    # pickled = (X, Y)
+    pickled = (X, Y)
     filename = preprocessed_data_file
 
-    # with open(filename, 'wb') as f:
-    #     pickle.dump(pickled, f)
+    with open(filename, 'wb') as f:
+        pickle.dump(pickled, f)
 
     with open(filename, 'rb') as f:
         X, Y = pickle.load(f)
 
-    doc = codecs.open(labels_file,'rU','UTF-8')
-    labels = pd.read_csv(doc, sep= '\t', lineterminator='\n')
-    print(labels['eye_color'].value_counts())
+    
