2231cacacebf14f4e16ad875c09f4ad6d5a4d81e
nour.khaled <nour.khaled@arup.com>
2020-01-13 15:06:56 +0000

    Tidy up the script

--- A1/gender_classifier.py
+++ A1/gender_classifier.py
@@ -4,9 +4,9 @@ import pickle
 import numpy as np
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from sklearn.model_selection import GridSearchCV
 from sklearn.model_selection import train_test_split
 from sklearn.svm import SVC
-from sklearn.model_selection import GridSearchCV
 
 import A1.lab2_landmarks as l2
 
@@ -21,19 +21,8 @@ def transform_images_to_features_data(csv_file_path, gender_column_index, image_
 def split_training_test_data(X, Y, testsize):
     return train_test_split(X, Y, test_size=testsize)
 
-# sklearn functions implementation
 
-def img_SVM(training_images, training_labels, test_images, test_labels):
-
-    classifier = SVC(kernel='poly', gamma='scale')
-    classifier.fit(training_images, training_labels)
-    pred = classifier.predict(test_images)
-    print("Accuracy:", accuracy_score(test_labels, pred))
-    print(classification_report(test_labels, pred))
-
-    return pred
-
-
+# apply grid search and cross validation to find the optimal hyperparameter combination of the training model
 def img_SVM_GS_CV(training_images, training_labels, test_images, test_labels):
     parameter_candidates = [
         {'C': [1, 10, 100], 'gamma': [0.001, 0.0001], 'kernel': ['rbf']},
@@ -58,6 +47,16 @@ def img_SVM_GS_CV(training_images, training_labels, test_images, test_labels):
     print(classification_report(y_true, y_pred))
     print()
 
+# sklearn functions implementation
+# SVM model with the optimal hyperparameters combination found by gridsearch and cross validation
+def img_SVM(training_images, training_labels, test_images, test_labels):
+    classifier = SVC(kernel='rbf', gamma=0.0001, C=100)
+    classifier.fit(training_images, training_labels)
+    pred = classifier.predict(test_images)
+    print("Accuracy:", accuracy_score(test_labels, pred))
+    print(classification_report(test_labels, pred))
+
+    return pred
 
 
 if __name__ == '__main__': ## command J
@@ -95,23 +94,16 @@ if __name__ == '__main__': ## command J
     with open(filename, 'rb') as f:
         X, Y = pickle.load(f)
 
+    # split the data into 70% taiining, 15% validation and 15% test sets
+
     X_train, X_test, Y_train, Y_test = split_training_test_data(X, Y, 0.3)
     X_test, X_val, Y_test, Y_val = split_training_test_data(X_test, Y_test, 0.5)
 
-    # pickled_train = (X_train, Y_train)
-    # pickled_val = (X_val, Y_val)
-    # pickled_test = (X_test, Y_test)
-    #
-    # filename_train = 'pickled_train'
-    # with open(filename_train, 'wb') as f:
-    #     pickle.dump(pickled_train, f)
-    #
-    # filename_val = 'pickled_val'
-    # with open(filename_val, 'wb') as f:
-    #     pickle.dump(pickled_val, f)
-    #
-    # filename_test = 'pickled_test'
-    # with open(filename_test, 'wb') as f:
-    #     pickle.dump(pickled_test, f)
+    # img_SVM_GS_CV is used to train the model
+    pred = img_SVM_GS_CV(X_train.reshape((3360, 68*2)), list(zip(*Y_train))[0], X_val.reshape((720, 68*2)), list(zip(*Y_val))[0])
 
-    # pred = img_SVM_GS_CV(X_train.reshape((3360, 68*2)), list(zip(*Y_train))[0], X_val.reshape((720, 68*2)), list(zip(*Y_val))[0])
+    # img_SVM is used to test the model
+    pred_test = img_SVM(X.reshape((4800, 68*2)), list(zip(*Y))[0], X_test.reshape((720, 68*2)), list(zip(*Y_test))[0])
+
+
+
